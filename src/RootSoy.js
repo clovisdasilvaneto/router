ec968d8072bc829026d3414f6a49be2309629c06
Matuzalém S. Teles <matuzalemteles@gmail.com>
2017-08-18 18:02:09 -0300

    WIP

---
+++ src/RootSoy.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import Component from 'metal-component';
+import Soy from 'metal-soy';
+import templates from './RootSoy.soy.js';
+
+// Import`s components
+import './BrowserRouter.js'
+
+class RootSoy extends Component {
+}
+
+Soy.register(RootSoy, )
+
+export { RootSoy };
+export default RootSoy;
